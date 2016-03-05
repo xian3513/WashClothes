ff98e4da8ab3a96a7a82d898f6662a1714107f57
zzx <23090284@qq.com>
2016-03-05 13:13:30 +0800

    wx

--- washClothes.php
+++ washClothes.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+	</head>
+	<body>
 <?PHP
     echo 'di jun ------>  sb';
+
+    $conn = mysql_connect("SAE_MYSQL_HOST_M:SAE_MYSQL_PORT","SAE_MYSQL_USER","SAE_MYSQL_PASS");
+    if($conn){
+    	echo "ok";
+    }else {
+    	echo "fail";
+    }
 ?>
+	</body>
+
+</html>
